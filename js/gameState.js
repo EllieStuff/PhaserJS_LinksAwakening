c52240f1f2fe50c4bf998d2a72dd65f420ef3be6
Joel Mateu <joel.mateu@enti.cat>
2020-11-18 19:47:00 +0100

    Sprites and basic stuff

---
+++ js/gameState.js
@@ -0,0 +1,45 @@
+class gameState extends Phaser.Scene{
+	constructor(){
+		//Llamamos al constructor de la escena
+	 	super({key: 'gameState'});
+	}
+	preload(){
+        //Load Images
+        
+        //Load Audios
+        
+        
+	}
+	create(){
+        //SetOrigin
+		
+ 	
+        //add.sprite & anims.create
+        
+        
+        //Variables
+        
+        
+        //LoadGroups
+		
+        
+        //time.addEvent
+        
+        
+        //Colisiones
+		
+        
+	}
+    
+    
+    //Functions
+    
+    
+    
+    //
+    
+	update(){
+        
+        
+	}
+}
