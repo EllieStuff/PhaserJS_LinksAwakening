2fe983484ffba3a2382119918090ee8b891fc944
David Martín <david.martin@enti.cat>
2020-12-13 19:11:31 +0100

    Movimiento basico en un eje

--- js/Enemies/BladePrefab.js
+++ js/Enemies/BladePrefab.js
@@ -9,27 +9,40 @@ class BladePrefab extends EnemyBase{
         this.seeRange = 100;
         this.speed = 100;
         this.collided = false;
-        this.initPositionX = positionX;
-        this.initPositionY = positionY;
+        this.initPosition = new Phaser.Math.Vector2(positionX, positionY);
+        this.targetPosRight = new Phaser.Math.Vector2(this.initPosition.x + 1 * 100, this.initPosition.y);
+        this.returning = false;
+        
+        this.epsilon = 7;
+        this.stopped = true;
     } 
     
     Update()
     {
         var currentPos = new Phaser.Math.Vector2(this.body);
-        var dir = this.scene.player.body - currentPos;
+        var dir = new Phaser.Math.Vector2(1,0/*this.scene.player.body.x - currentPos.x,this.scene.player.body.y - currentPos.y*/);
+        var dist = 10;
+        //var isInLine = (dir.normalize == Phaser.Math.Vector2(1,0)) || (dir.normalize == Phaser.Math.Vector2(-1,0)) || (dir.normalize == Phaser.Math.Vector2(0,1)) || (dir.normalize == Phaser.Math.Vector2(0,-1));
         
-        var isInLine = (dir.normalize == Phaser.Math.Vector2(1,0)) || (dir.normalize == Phaser.Math.Vector2(-1,0)) || (dir.normalize == Phaser.Math.Vector2(0,1)) || (dir.normalize == Phaser.Math.Vector2(0,-1));
-        
-        
-        if(currentPos.distance(this.scene.player.body) < this.seeRange && !this.collided && isInLine)
-        {
-            //this.MoveTowards(this.scene.player, this.speed);
-            this.body.setVelocity(dir.x*this.speed, dir.y*this.speed);
+        if(currentPos.distance(this.targetPosRight) < this.epsilon && !this.returning){
+            this.returning = true;
+            this.stopped = false;
         }
-        else if (!this.collided)
-        {
+        else if(currentPos.distance(this.initPosition) < this.epsilon && this.returning){
+            this.returning = false;
             this.body.stop();
         }
         
+        if(!this.returning && this.stopped)
+        {
+            this.MoveTowards(this.targetPosRight, this.speed);
+        }
+        else if (this.returning)
+        {
+            this.MoveTowards(this.initPosition, this.speed/2);
+        }
+        if(this.collided){
+            this.body.stop();
+        }
     }
 }
