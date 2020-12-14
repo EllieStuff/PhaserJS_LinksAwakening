262df76ad04d811143bca4e1e0dd5e8f2392c9be
David Martín <david.martin@enti.cat>
2020-12-14 23:43:33 +0100

    Cuchillitas acabado

--- js/Enemies/BladePrefab.js
+++ js/Enemies/BladePrefab.js
@@ -11,37 +11,70 @@ class BladePrefab extends EnemyBase{
         this.collided = false;
         this.initPosition = new Phaser.Math.Vector2(positionX, positionY);
         this.targetPosRight = new Phaser.Math.Vector2(this.initPosition.x + 1 * 100, this.initPosition.y);
+        this.targetPosLeft = new Phaser.Math.Vector2(this.initPosition.x - 1 * 100, this.initPosition.y);
+        this.targetPosDown = new Phaser.Math.Vector2(this.initPosition.x, this.initPosition.y + 1 * 100);
+        this.targetPosUp = new Phaser.Math.Vector2(this.initPosition.x, this.initPosition.y - 1 * 100);
         this.returning = false;
         
-        this.epsilon = 7;
+        this.epsilon = 10;
         this.stopped = true;
     } 
     
     Update()
     {
         var currentPos = new Phaser.Math.Vector2(this.body);
-        var dir = new Phaser.Math.Vector2(1,0/*this.scene.player.body.x - currentPos.x,this.scene.player.body.y - currentPos.y*/);
-        var dist = 10;
-        //var isInLine = (dir.normalize == Phaser.Math.Vector2(1,0)) || (dir.normalize == Phaser.Math.Vector2(-1,0)) || (dir.normalize == Phaser.Math.Vector2(0,1)) || (dir.normalize == Phaser.Math.Vector2(0,-1));
         
-        if(currentPos.distance(this.targetPosRight) < this.epsilon && !this.returning){
+        if(currentPos.distance(this.targetPosDown) < this.epsilon && !this.returning){
+            this.returning = true;
+            this.stopped = false;
+        }
+        else if(currentPos.distance(this.targetPosRight) < this.epsilon && !this.returning){
+            this.returning = true;
+            this.stopped = false;
+        }
+        else if(currentPos.distance(this.targetPosLeft) < this.epsilon && !this.returning){
+            this.returning = true;
+            this.stopped = false;
+        }
+        else if(currentPos.distance(this.targetPosUp) < this.epsilon && !this.returning){
             this.returning = true;
             this.stopped = false;
         }
         else if(currentPos.distance(this.initPosition) < this.epsilon && this.returning){
             this.returning = false;
+            this.stopped = true;
             this.body.stop();
         }
         
         if(!this.returning && this.stopped)
         {
-            this.MoveTowards(this.targetPosRight, this.speed);
+            
+            if(currentPos.distance(this.scene.player.body) < this.seeRange){
+                var dir = new Phaser.Math.Vector2(this.scene.player.body.x - currentPos.x,this.scene.player.body.y - currentPos.y);
+                if(dir.x > dir.y && dir.x > 0){
+                    this.stopped = false;
+                    this.MoveTowards(this.targetPosRight, this.speed);
+                }
+                else if(dir.x < dir.y && dir.y > 0){
+                    this.stopped = false;
+                    this.MoveTowards(this.targetPosDown, this.speed);
+                }
+                else if(dir.x < dir.y && dir.y < 0){
+                    this.stopped = false;
+                    this.MoveTowards(this.targetPosLeft, this.speed);
+                }
+                else if(dir.x > dir.y && dir.x < 0){
+                    this.stopped = false;
+                    this.MoveTowards(this.targetPosUp, this.speed);
+                }
+            }
         }
         else if (this.returning)
         {
             this.MoveTowards(this.initPosition, this.speed/2);
         }
         if(this.collided){
+            this.stopped = true;
             this.body.stop();
         }
     }
