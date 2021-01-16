d53bfa1ff7324abea81eb9af44f84ca758037a30
Victor Garcinuño <victorgarcinuno@gmail.com>
2021-01-16 14:01:14 +0100

    haciendo cosas

--- js/CameraManager.js
+++ js/CameraManager.js
@@ -3,15 +3,19 @@ class CameraManager
     
     constructor(_scene)
     {
+        this.scene = _scene
         _scene.events.on('update', this.Update, this);
+        _scene.cameras.main.setBounds(0, 0, config.width, config.height);
+        
         
         this.TileX = 3;
         this.TileY = 5;
-        this.scene = _scene
-        this.scene.cameras.main.setBounds(0, 0, this.scene.config.width, this.scene.config.height);
+        
+        
         this.camPosX = 160 * this.TileX;
         this.camPosY = 128 * this.TileY;
-        this.scene.cameras.main.centerOn(this.camPosX + this.scene.config.width/2,this.camPosY + this.scene.config.height/2);
+        this.scene.cameras.main
+        this.scene.cameras.main.centerOn(this.camPosX + config.width/2, this.camPosY + config.height/2);
         
     }
     
@@ -27,34 +31,36 @@ class CameraManager
     
     Update()
     {
+        /*
         if(this.scene.player.body.position.x < this.camPosX)
         {
             this.camPosX -= 160;
-            this.scene.cameras.main.centerOn(this.camPosX + this.scene.width/2,this.camPosY + this.scene.height/2);
+            this.scene.cameras.main.centerOn(this.camPosX + config.width/2,this.camPosY + config.height/2);
             this.TileX--;
             this.GenerateEnemies();
         }
         else if(this.scene.player.body.position.x > this.camPosX + 160)
         {
            this.camPosX += 160;
-           this.scene.cameras.main.centerOn(this.camPosX + this.scene.width/2,this.camPosY + this.scene.height/2);
+           this.scene.cameras.main.centerOn(this.camPosX + config.width/2,this.camPosY + config.height/2);
             this.TileX++;
             this.GenerateEnemies();
         }
         else if(this.scene.player.body.position.y < this.camPosY)
         {
             this.camPosY -= 128;
-            this.scene.cameras.main.centerOn(this.camPosX + this.scene.width/2,this.camPosY + this.scene.height/2);
+            this.scene.cameras.main.centerOn(this.camPosX + config.width/2,this.camPosY + config.height/2);
             this.TileY--;
             this.GenerateEnemies();
         }
         else if(this.scene.player.body.position.y > this.camPosY + 128)
         {
             this.camPosY += 128;
-            this.scene.cameras.main.centerOn(this.camPosX + this.scene.width/2,this.camPosY + this.scene.height/2);
+            this.scene.cameras.main.centerOn(this.camPosX + config.width/2,this.camPosY + config.height/2);
             this.TileY++;
             this.GenerateEnemies();
         }   
+        */
     }
     
 }
